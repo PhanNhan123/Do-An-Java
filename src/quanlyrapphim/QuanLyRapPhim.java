cefd252ccecede369e2de48f6656aed83355bc08
Phan Nhân <phannhan@Phans-MacBook-Pro.local>
2019-05-23 09:47:15 +0700

    first commit

---
+++ src/quanlyrapphim/QuanLyRapPhim.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//package quanlyrapphim;
+//
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//
+//import GUI.QuanLyVe;
+//import GUI.Home;
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+//import java.sql.SQLException;
+//import javax.swing.*;
+///**
+// *
+// * @author Dell
+// */
+//public class QuanLyRapPhim {
+//
+//    /**
+//     * @param args the command line arguments
+//     */
+//    public static void main(String[] args) throws SQLException {
+//        // TODO code application logic here
+////        String dbURL = "jdbc:sqlserver://localhost:1433;databaseName=QLRP;user=sa;password=sa";
+////Connection conn = DriverManager.getConnection(dbURL);
+////if (conn != null) {
+//// System.out.println("Connected");
+////}       
+//         QuanLyVe QLV=new QuanLyVe();
+//        QLV.CreateAndShow();
+//        //Home home=new Home();
+//        //home.CreateAndShow();
+//    }
+//    
+//}
---
+++ src/util/DataAccessHelper.java
@@ -0,0 +1,65 @@
+package util;
+
+import java.sql.*;
+
+public class DataAccessHelper {
+    public Connection conn = null;
+
+
+    public void displayError(SQLException ex) {
+        System.out.println("Error Message: " + ex.getMessage());
+        System.out.println("Error state: " + ex.getSQLState());
+        System.out.println("Error Code: " + ex.getErrorCode());
+    }
+
+    public void open() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager
+                    .getConnection("jdbc:mysql://localhost:3306/QLRP","root","PTNhan01653817474");  
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void close() {
+        try {
+            if(conn != null) {
+                conn.close();
+            }
+        }catch (SQLException ex) {
+            displayError(ex);
+        }
+    }
+
+    public ResultSet excuteQuery(String sql) {
+        ResultSet rs = null;
+        try {
+            Statement statement = conn.createStatement();
+            rs = statement.executeQuery(sql);
+        } catch (SQLException ex) {
+            displayError(ex);
+        }
+        return rs;
+    }
+
+    public static void main(String[] args) {
+        DataAccessHelper helper = new DataAccessHelper();
+        String sql = "SELECT * FROM NhanVien";
+        helper.open();
+        ResultSet rs = helper.excuteQuery(sql);
+        
+        try {
+            
+        while(rs.next()) {
+            System.out.println(rs.getString("MaNV"));
+            System.out.println(rs.getString("TenNV"));
+            
+        }
+        } catch(SQLException e) {
+            helper.displayError(e);
+        }
+        
+        helper.close();
+    }
+}
